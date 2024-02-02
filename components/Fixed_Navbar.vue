da5de36282d085a9e984a755dc721947fd72bc4d
roxgiba <rox.giba@gmail.com>
2024-02-02 09:16:51 +0100

    updated component fixed_navbar

--- components/Fixed_Navbar.vue
+++ components/Fixed_Navbar.vue
@@ -3,35 +3,14 @@ import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
 </script>
 
 <template>
- <!-- <section class="menu">
+  <section class="menu">
     <div class="dropdown">
       <div>Locations</div>
       <div>Blog</div>
       <div>Rates</div>
     </div>
     <div class="language">UK|EUR</div>
-  </section> -->
-
-  <div class="menu">
-    <b-dropdown class="dropdown m-md-2" id="dropdown-1" text="Motorhome Hire">
-    <b-dropdown-item href="#">Campervan Hire</b-dropdown-item>
-    <b-dropdown-item href="#">Motorhome Hire</b-dropdown-item>
-    <b-dropdown-item href="#"><a href="">All campervans & motorhomes</a></b-dropdown-item>
-  </b-dropdown>
-    
-  <b-dropdown class="dropdown m-md-2" id="dropdown-1" text="Locations">
-    <b-dropdown-item href="#">List of countries</b-dropdown-item>
-  </b-dropdown>
-
-    <b-dropdown class="dropdown m-md-2" id="dropdown-1" text="Blog">
-    <b-dropdown-item href="#">Locations</b-dropdown-item>
-    <b-dropdown-item href="#">Blog</b-dropdown-item>
-    <b-dropdown-item href="#">Rates</b-dropdown-item>
-  </b-dropdown>
-
-    <b-dropdown class="dropdown m-md-2" id="dropdown-1" text="Rates">
-  </b-dropdown>
-</div>
+  </section>
 </template>
 
 <style>
